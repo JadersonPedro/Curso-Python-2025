0696602103321682562b5c4a1f91fb340360ce9f
jaderson <jadersonengbio@gmail.com>
2025-06-29 23:26:24 -0300

    Lidando com arquivos

---
+++ Dia_07/01_read.py
@@ -0,0 +1,23 @@
+#%%
+
+nome_arquivo = "historia.txt"
+
+with open(nome_arquivo) as open_file:
+    conteudo = open_file.read()
+
+print(conteudo)
+
+
+#Abre arquivo em formato de leitura
+#open_file = open(nome_arquivo)
+
+#Lê os dados do arquivo
+#conteudo = open_file.read()
+
+#print(conteudo)
+
+#Fecha o arquivo
+#open_file.close()
+
+
+# %%
